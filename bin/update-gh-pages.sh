af7c84ca3e8979298d03833db8ac5c002b7d2ee4
Jordan Woehr <jordanwoehr@gmail.com>
2018-09-23 08:29:02 -0600

    Documentation generation script

---
+++ bin/update-gh-pages.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p stack
+
+set -e
+
+stack haddock
+git checkout gh-pages
+# stack leaves things in these directories, so they stick around.
+rm -rf open-adt open-adt-tutorial
+git rm -r ./*
+cp -r .stack-work/install/x86_64-linux-nix/lts-12.9/8.4.3/doc/* .
+git add .
+echo "Verify changes before committing and pushing. :)"
+echo "And remember you are on the gh-pages branch now."
