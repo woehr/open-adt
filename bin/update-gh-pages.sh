e340bda1444dafd0a7621c58559dfa35bfd071eb
Jordan Woehr <jordan.woehr@hubh2o.com>
2019-04-11 13:14:40 -0600

    Fixes

--- bin/update-gh-pages.sh
+++ bin/update-gh-pages.sh
@@ -9,7 +9,7 @@ git checkout gh-pages
 rm -rf open-adt/.stack-work open-adt-tutorial/.stack-work
 rmdir open-adt open-adt-tutorial
 git rm -r ./*
-cp -r .stack-work/install/x86_64-linux-nix/lts-12.9/8.4.3/doc/* .
+cp -r .stack-work/install/x86_64-linux-nix/lts-12.26/8.4.4/doc/* .
 git add .
 echo "Verify changes before committing and pushing. :)"
 echo "And remember you are on the gh-pages branch now."
